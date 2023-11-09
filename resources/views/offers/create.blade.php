91f930c069498236ef81d9716c663e05c681ef89
Filip Kniaś <filipknias@MacBook-Air-Filip.local>
2023-11-09 17:32:17 +0100

    laravel setup with tailwind

---
+++ resources/views/offers/create.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h1 class="text-center text-4xl py-12 font-medium">Create offer</h1>
+@stop 
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+    <h1 class="text-center text-4xl py-12 font-medium">Welcome page</h1>
+@stop    
+        
